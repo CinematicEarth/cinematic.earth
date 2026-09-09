3809b7f250cdedc263b6c81040a6a88ec2949ca2
Garrett Brown <eigendebugger@gmail.com>
2026-09-08 19:37:13 -0700

    Update stream URL to Real Relaxation Cinematic.Earth Jazz

--- src/app/launchApp.ts
+++ src/app/launchApp.ts
@@ -27,7 +27,7 @@ export function launchApp(): void {
   }
 
   const streamUrl: string =
-    "https://stream.mux.com/dDkIbyl402OA1QkR3CgEMVUQltsjzF1ulB4579ff7sB8.m3u8";
+    "https://stream.mux.com/yFBmaXVr9es3vV4uC01iYHc39WfGE9kghyYvaUf017BfU.m3u8";
   let shakaPlayer: shaka.Player | null = null;
   let streamLoaded: boolean = false;
 
